d3b34d7a3af021cc53cefba2ed0e682c14aa179c
VictorGHss <vg.hss0101@gmail.com>
2026-03-04 15:21:17 -0300

    fix: remove upload size warnings

--- api/src/main/java/br/dev/ctrls/inovareti/core/exception/FileSizeLimitExceededException.java
+++ api/src/main/java/br/dev/ctrls/inovareti/core/exception/FileSizeLimitExceededException.java
@@ -1,9 +1,5 @@
 package br.dev.ctrls.inovareti.core.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
 public class FileSizeLimitExceededException extends RuntimeException {
 
     public FileSizeLimitExceededException(String message) {

c9a5192219e81d72932cf9f9fbe5853ffb0793ed
VictorGHss <vg.hss0101@gmail.com>
2026-03-11 14:38:14 -0300

    fix: forca inicializacao programatica do flyway no startup

--- api/src/main/java/br/dev/ctrls/inovareti/config/FlywayConfig.java
+++ api/src/main/java/br/dev/ctrls/inovareti/config/FlywayConfig.java
@@ -1,22 +1,38 @@
 package br.dev.ctrls.inovareti.config;
 
 import javax.sql.DataSource;
+
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FlywayConfig {
 
-    // O initMethod faz a migração rodar no exato momento que o Bean nasce
-    @Bean(initMethod = "migrate")
+    @Bean
     public Flyway flyway(DataSource dataSource) {
-        System.out.println(">>> [FLYWAY] INICIANDO MIGRATIONS ANTES DO HIBERNATE <<<");
-        
-        return Flyway.configure()
+        Flyway flyway = Flyway.configure()
                 .dataSource(dataSource)
                 .locations("classpath:db/migration")
                 .baselineOnMigrate(true)
                 .load();
+        flyway.migrate();
+        return flyway;
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor entityManagerFactoryDependsOnFlyway() {
+        return beanFactory -> forceDependsOnFlyway(beanFactory);
+    }
+
+    private static void forceDependsOnFlyway(ConfigurableListableBeanFactory beanFactory) {
+        if (!beanFactory.containsBeanDefinition("entityManagerFactory")) {
+            return;
+        }
+
+        var entityManagerFactory = beanFactory.getBeanDefinition("entityManagerFactory");
+        entityManagerFactory.setDependsOn("flyway");
     }
 }

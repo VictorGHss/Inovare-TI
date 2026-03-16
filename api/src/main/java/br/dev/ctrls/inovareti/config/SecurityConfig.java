204cb05ab65c5404e85a85efb356ee7fbcd19bc7
VictorGHss <vg.hss0101@gmail.com>
2026-03-16 11:30:35 -0300

    fix: corrige caminhos no SecurityConfig ignorando o context-path para liberar Conta Azul

--- api/src/main/java/br/dev/ctrls/inovareti/config/SecurityConfig.java
+++ api/src/main/java/br/dev/ctrls/inovareti/config/SecurityConfig.java
@@ -55,8 +55,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/reset-initial-password").permitAll()
                 .requestMatchers(HttpMethod.GET, "/attachments/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/financeiro/contaazul/authorize").permitAll()
-                .requestMatchers(HttpMethod.GET, "/financeiro/contaazul/callback").permitAll()
+                .requestMatchers("/financeiro/contaazul/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);

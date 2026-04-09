d996826a4147eb7c6fc0f951e859711ee44c8a9f
VictorGHss <vg.hss0101@gmail.com>
2026-04-09 16:45:36 -0300

    Ajusta envio de e-mails e layout do recibo interno

--- api/src/main/java/br/dev/ctrls/inovareti/domain/notification/FinanceEmailService.java
+++ api/src/main/java/br/dev/ctrls/inovareti/domain/notification/FinanceEmailService.java
@@ -152,8 +152,7 @@ public class FinanceEmailService {
                 + "<p>Prezado(a) " + safeName + ",</p>"
                 + "<p>Confirmamos o recebimento do valor referente à Venda " + safeSaleNumber
                 + ". O seu recibo de quitação já está disponível e segue em anexo a este e-mail.</p>"
-                + "<p>Agradecemos a confiança.</p>"
-                + "<p>Atenciosamente,<br/>Equipe Administrativa<br/>Inovare Serviços de Saúde</p>"
+            + "<p>Atenciosamente,<br/>Administrativo Inovare</p>"
                 + "</body></html>";
     }
 

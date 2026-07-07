8549114e4c0a5f3bf0e8e8c20f1444d104e283d7
VictorGHss <vg.hss0101@gmail.com>
2026-07-07 09:03:14 -0300

    fix: adiciona polyfill de global para window no vite.config.ts para evitar crash do sockjs-client

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -7,6 +7,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  define: {
+    global: 'window',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
